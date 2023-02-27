7ff05f12495e4f686fcf7761581ca091e31d217a
Otabek <otabek@gmail.com>
2023-02-27 15:49:04 +0500

    changes

--- src/main/java/mobile/information/interceptor/ErrorAdvice.java
+++ src/main/java/mobile/information/interceptor/ErrorAdvice.java
@@ -6,7 +6,7 @@ import mobile.information.model.container.ResponseContainer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
+/*import org.springframework.security.core.context.SecurityContextHolder;*/
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,7 +16,7 @@ public class ErrorAdvice {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> get(RuntimeException e) {
-        var name = SecurityContextHolder.getContext().getAuthentication().getName();
+        /*var name = SecurityContextHolder.getContext().getAuthentication().getName();*/
         var error = new ResponseContainer(ResultCode.internal_server, "");
         logger.error(e.getMessage(), e);
         if (e instanceof BaseException ex) {
